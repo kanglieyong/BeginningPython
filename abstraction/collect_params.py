16f8cb621b408fbd98534f9b4f508cceca1c010f
kanglieyong <kanglieyong@gmail.com>
2017-05-24 23:18:26 +0800

    collect params: much more complicated

--- abstraction/collect_params.py
+++ abstraction/collect_params.py
@@ -14,3 +14,10 @@ def print_params_3(**params):
     print(params)
 
 print_params_3(x=1, y=2, z=3)
+
+def print_params_4(x, y, z=3, *pospar, **keypar):
+    print(x, y, z)
+    print(pospar)
+    print(keypar)
+
+print_params_4(1,2,3, 5,6,7, foo=1,bar=2)
