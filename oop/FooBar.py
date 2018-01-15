529587d99b3bfa4dfb98e85f78d06ef05d6df74d
kanglieyong <kanglieyong@gmail.com>
2018-01-16 00:02:27 +0800

    magic method

--- oop/FooBar.py
+++ oop/FooBar.py
@@ -4,3 +4,10 @@ class FooBar:
 
 f = FooBar()
 print(f.somevar)
+
+class FooBaz:
+  def __init__(self, value=42):
+    self.somvar = value
+
+g = FooBaz("This is a constructor argument")
+print(g.somvar)
