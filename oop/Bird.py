32764993337617866a14d3e32533f5eea8c2336d
kanglieyong <kanglieyong@gmail.com>
2018-01-16 00:09:25 +0800

    override

---
+++ oop/Bird.py
@@ -0,0 +1,21 @@
+class Bird:
+  def __init__(self):
+    self.hungry = True
+  def eat(self):
+    if self.hungry:
+      print("Aaahh...")
+      self.hungry = False
+    else:
+      print("No, thanks")
+
+class SongBird(Bird):
+  def __init__(self):
+    self.sound = "Squawk!"
+  def sing(self):
+    print(self.sound)
+
+b = Bird()
+b.eat()
+
+sb = SongBird()
+sb.sing()
